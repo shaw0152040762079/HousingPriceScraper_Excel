6d138577399ae0402179ef455e313ab9aa0a66c9
shaw0152040762079 <shaw0152@algonquinlive.com>
2020-11-22 20:10:48 -0500

    Simple WebScraper for housing prices in Canada

--- webScraperHouses.py
+++ webScraperHouses.py
@@ -33,7 +33,7 @@ url = 'https://www.point2homes.com/CA/Real-Estate-Listings/' + Province + '/' + 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
 
 
-page = requests.get(url + str(1),headers=headers)
+page = requests.get(url,headers=headers)
 print(page.content)
 soup = BeautifulSoup(page.content, 'html.parser')
 number_of_houses = soup.find(id="search_history_form")
