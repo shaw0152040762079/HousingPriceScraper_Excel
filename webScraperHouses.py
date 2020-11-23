a22a9105d1f5b62c9c021fe3cf8707b4b457f45a
shaw0152040762079 <shaw0152@algonquinlive.com>
2020-11-22 21:14:31 -0500

    Improved the String Url arguments

--- webScraperHouses.py
+++ webScraperHouses.py
@@ -19,17 +19,17 @@ house = Prompt.ask(
     choices=["CondoApartment", "House", "RowTownhouse", ]
 )
 
-Province = Prompt.ask(
+province = Prompt.ask(
     "What province are you looking in? enter the corresponding code",
     choices=["ON", "QC", 'AB', 'BC', 'MB', 'NB', 'NL', 'NT', 'NS', 'NU', 'PE', 'SK', 'YT']
 )
 
-City = Prompt.ask(
-    "Please Enter the City",
+city = Prompt.ask(
+    "Please Enter the city",
 )
 
 prices = []
-url = 'https://www.point2homes.com/CA/Real-Estate-Listings/' + Province + '/' + City + '.html?location=Ottawa,%20ON&PropertyType=' + house + '&search_mode=location&SelectedView=listings&LocationGeoId=783093&page='
+url = 'https://www.point2homes.com/CA/Real-Estate-Listings/' + province + '/' + city + '.html?location=' + city +'PropertyType=' + house + '&search_mode=location&SelectedView=listings&page='
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
 
 # may need to change URL if throttled!!!
@@ -38,10 +38,11 @@ soup = BeautifulSoup(page.content, 'html.parser')
 number_of_houses = soup.find(id="search_history_form")
 number = number_of_houses.find(id='search-history-results')
 
-pages = int(int(number['value']) / 24)
+pages = int(int(number['value']) // 24)
 
 for i in range(pages):
     page = requests.get(url + str(i),headers=headers)
+    print(url+str(i))
     soup = BeautifulSoup(page.content, 'html.parser')
     pagenum = 1
     with Bar('Downloading new page', fill='@', suffix='%(percent).1f%% - %(eta)ds') as bar:
@@ -55,21 +56,20 @@ for i in range(pages):
 
 
 print('/n')
-total = 0
-for price in prices:
-    total += price
+
+total = sum(prices)
 
 print(prices.sort())
 Average_Price = total / len(prices)
 
 property_table = Table()
-property_table.add_column('[bold green]City[/bold green]', style="bold cyan")
+property_table.add_column('[bold green]city[/bold green]', style="bold cyan")
 property_table.add_column("House Type")
 property_table.add_column("Average Price")
 property_table.add_column("Median Price")
 property_table.add_column("Sample Size")
 
-property_table.add_row(City, '[bold cyan]' + house + '[/bold cyan]', "[bold red]" + str(Average_Price),
+property_table.add_row(city, '[bold cyan]' + house + '[/bold cyan]', "[bold red]" + str(Average_Price),
                        str(statistics.median(prices)), str(len(prices)))
 
 console.print(property_table)
